6ccf7fde2d7591da760ad05d4efbf21eee4a3e7d
nathan yebgui <nathan_yebgui@msn.com>
2018-10-07 16:38:27 +0300

    Added controller, actions, view and viewnodel for registering a user.

---
+++ Treehouse.FitnessFrog/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Treehouse.FitnessFrog.ViewModels;
+
+namespace Treehouse.FitnessFrog.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(AccountRegisterViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
---
+++ Treehouse.FitnessFrog/ViewModels/AccountRegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Treehouse.FitnessFrog.ViewModels
+{
+    public class AccountRegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [StringLength(100, MinimumLength =6,ErrorMessage = "the {0} must be at least {2} characters long." )]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and Confirmation Password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
