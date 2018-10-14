3a2ab6146459cb48a8d01aaf98d50f8607768375
nathan yebgui <nathan_yebgui@msn.com>
2018-10-14 16:28:13 +0300

    Added the Startup class for Owin, and ConfigureAuth method for Cookie based identity

---
+++ Treehouse.FitnessFrog/App_Start/Startup.Auth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+
+namespace Treehouse.FitnessFrog
+{
+	public partial class Startup
+	{
+		private void ConfigueAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/SignIn"),
+                Provider = new CookieAuthenticationProvider()
+            });
+        }
+	}
+}
---
+++ Treehouse.FitnessFrog/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Treehouse.FitnessFrog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigueAuth(app);
+        }
+    }
+}
